3513cf04c2ee2685b268e910c9166f6c786fa06a
HARJAPAN120405 <harjapan@heliverse.com>
2026-03-17 01:00:31 +0530

    Fix Vercel build: explicit .js import and @types/node

--- api/[[...path]].ts
+++ api/[[...path]].ts
@@ -1,5 +1,5 @@
 /**
  * Vercel serverless entry: all /api/* requests are handled by the Hono app.
  */
-import { app } from '../server/index';
+import { app } from '../server/index.js';
 export default app;
